633efbc3c8c5d97e10d4958bad477cbff860ab03
Juca Crispim <jucacrispim@gmail.com>
2024-05-23 00:00:30 -0300

    Fix docs upload

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,4 +9,4 @@ cd dist
 fname=`ls | grep tar`
 project_name=toxiccore
 
-curl -F file=@$fname -F prefix=pypi/$project_name $PYPI_UPLOAD_URL -H "Authorization: Key $TUPI_AUTH_KEY"
+curl -F file=@$fname -F prefix=$project_name https://pypi.poraodojuca.dev/u/ -H "Authorization: Key $PYPI_AUTH_KEY"
