b22259c650c95f2373a5af5bd6c625335f52b5c2
Juca Crispim <jucacrispim@gmail.com>
2024-05-22 21:51:17 -0300

    Fix release name

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,6 @@ python -m build
 cd dist
 
 fname=`ls | grep tar`
-project_name=toxiccore
+project_name=toxicbuild-core
 
 curl -F file=@$fname -F prefix=pypi/$project_name $PYPI_UPLOAD_URL -H "Authorization: Key $TUPI_AUTH_KEY"
