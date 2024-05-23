a4a34fa9445074e240309d21d6aed36c73b2c940
Juca Crispim <jucacrispim@gmail.com>
2024-05-22 21:56:39 -0300

    Change project name for packaging

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,6 @@ python -m build
 cd dist
 
 fname=`ls | grep tar`
-project_name=toxicbuild-core
+project_name=toxiccore
 
 curl -F file=@$fname -F prefix=pypi/$project_name $PYPI_UPLOAD_URL -H "Authorization: Key $TUPI_AUTH_KEY"
