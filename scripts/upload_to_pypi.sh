2710f99a1dd4e7237e80421fddd0496b62c8aa0b
Juca Crispim <juca@poraodojuca.dev>
2025-10-09 21:50:51 -0300

    Add --verbose to twine upload

--- scripts/upload_to_pypi.sh
+++ scripts/upload_to_pypi.sh
@@ -4,7 +4,7 @@ confpath=~/.pypirc
 echo -e "[pypi]\nusername = $PYPI_USERNAME\npassword = $PYPI_PASSWORD" > $confpath
 cd dist
 fname=`ls | grep tar`
-twine upload -r pypi $fname
+twine upload -r pypi $fname --verbose
 r=$?
 rm ~/.pypirc
 exit $r
