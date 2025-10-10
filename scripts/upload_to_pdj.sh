2a10ea1d533c9f27cd5eb1e48332fb9bfc94b01a
Juca Crispim <juca@poraodojuca.dev>
2025-10-09 22:20:19 -0300

    Fix package upload
    ci: include-builders Docs & Release

--- scripts/upload_to_pdj.sh
+++ scripts/upload_to_pdj.sh
@@ -3,6 +3,6 @@
 cd dist
 
 fname=`ls | grep tar`
-project_name=mongomotor
+project_name=toxiccore
 
 curl -F file=@$fname -F prefix=$project_name https://pypi.poraodojuca.dev/u/ -H "Authorization: Key $PYPI_AUTH_KEY"
